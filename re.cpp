b52d9a1eada53077f077c73bfe6b8e4896ab97d9
beef <samiaa4824@gmail.com>
2025-01-16 21:28:54 +0600

    changes

---
+++ re.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  int x;
+  cin >> x ;
+
+  switch(x){
+    case 15:
+    cout << "" << endl;
+    break;
+    case 40:
+    cout << "x is fine" << endl;
+    break;
+    default: 
+    cout <<  "x is fine but Sayanno gay" << endl;
+  }
+}
